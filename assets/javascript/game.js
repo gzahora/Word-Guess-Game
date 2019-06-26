d94de15f4940b14818ed47193177cec1f88df523
Greg Zahora <gregzahora3@gmail.com>
2019-06-26 17:53:57 -0400

    wrote basic pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//Writing Psuedo Code for step
+
+/*
+1) Have computer choose a word randomly from an array I provide
+2) Display that word as blank spaces until letters are guessed
+3) have user input guess
+4) show player their progress
+    4a) if they guess of a letter was correct, display the letters
+    4b) else if they guess the word, show the word and increase the score by one
+    4c) else tell the player they were wrong and decrease the number of guesses left
+*/
+
